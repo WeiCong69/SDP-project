86228413d6e5efea5a8e1a9080dbcdf794e9d79a
LAW WEI CONG <asarasensei@gmail.com>
2021-06-08 16:07:56 +0800

    boom

--- connection.php
+++ connection.php
@@ -1,10 +1,14 @@
 ﻿
 <?php  
 //the night is still young
+//Boomshakalaka
 $con= mysqli_connect("localhost","root","","huatgroup");
 
 if(mysqli_connect_errno())
 {
 	echo "Failed to connect to MySql.".mysqli_connect_error();
 }
+
+
+
 ?> 
